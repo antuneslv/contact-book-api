eb0c9bcc24a009aa4dc3bf0cb9f4b4cf24298391
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2026-07-27 11:23:46 -0300

    feat: adjust prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { PrismaService } from './database/prisma.service'
+import { PrismaModule } from './database/prisma.module'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
