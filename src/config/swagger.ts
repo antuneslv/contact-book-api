34e737082228935753394eb2cb11fc015314788a
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2026-07-27 17:59:07 -0300

    feat: add swagger and validation pipe setup

---
+++ src/config/swagger.ts
@@ -0,0 +1,21 @@
+import { INestApplication } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Contact Book API')
+    .setDescription('Contact book API with authentication')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { setupSwagger } from './config/swagger'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
+
+  setupSwagger(app)
+
   const env = app.get(EnvService)
 
   await app.listen(env.get('PORT'))
