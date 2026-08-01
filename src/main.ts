8270bc54f82398687dcf6d7076850eaf36573040
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2026-08-01 18:42:45 -0300

    feat: extract app setup from main

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,14 @@
-import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { setupApp } from './config/app'
 import { setupSwagger } from './config/swagger'
 import { EnvService } from './env/env.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix('api')
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  )
+  setupApp(app)
 
   setupSwagger(app)
 
